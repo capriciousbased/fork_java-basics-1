83379f03e2dcc89d6696efc895faca352bfac290
capriciousbased <mdanisulhoquekhan@gmail.com>
2022-12-09 15:06:02 +0100

    switch exercie 1

--- src/com/bt/java/basics/App.java
+++ src/com/bt/java/basics/App.java
@@ -5,13 +5,22 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        Scanner Obj = new Scanner(System.in);
-        String Name;
-
-        // Enter username and press Enter
-        System.out.println("Enter Name");
-        Name = Obj.nextLine();
-        System.out.println("Hello " + Name + ", welcome to AW Academy. ");
+        Scanner in = new Scanner(System.in);
+        int input = Integer.parseInt(in.nextLine());
+        switch (input) {
+            case 1:
+                System.out.println("Black");
+                break;
+            case 2:
+                System.out.println("White");
+                break;
+            case 3:
+                System.out.println("Red");
+                break;
+            case 4:
+                System.out.println("Green");
+                break;
+        }
 
     }
 
