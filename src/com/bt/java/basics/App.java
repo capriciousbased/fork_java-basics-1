037624b70e45b66865df9073529c27a59fe9d942
capriciousbased <mdanisulhoquekhan@gmail.com>
2022-12-09 13:26:29 +0100

    array test added

--- src/com/bt/java/basics/App.java
+++ src/com/bt/java/basics/App.java
@@ -4,6 +4,16 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
+        char[] name = { 'A', 'w', ' ', 'A', 'C', 'a', 'd', 'c', 'm', 'y' };
+
+        for (int i = 0; i < name.length; i++) {
+            System.out.println(name[i]);
+        }
+
+        for (int i = name.length - 1; i >= 0; i--) {
+            System.out.println(name[i]);
+        }
+
         for (int i = 0; i <= 100; i++) {
             if (i % 3 == 0) {
                 System.out.println("Fizz");
